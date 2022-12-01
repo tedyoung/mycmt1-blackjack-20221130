36699d592cf2d13c7859eab05f3f652348128bdc
Ted M. Young <tedyoung@gmail.com>
2022-12-01 15:54:26 -0800

    LAB #8: F - bet full wallet, isEmpty works

--- src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
@@ -28,6 +28,18 @@ public class WalletBettingTest {
         assertThat(wallet.balance())
                 .isEqualTo(27 - 7 - 9);
     }
+
+    @Test
+    void betFullBalanceThenWalletIsEmpty() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(73);
+
+        wallet.bet(73);
+
+        assertThat(wallet.isEmpty())
+                .isTrue();
+    }
+
 }
 
 
